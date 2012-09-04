e69d79914a2549d4186094721ba80b15c10a23fc
Wouter Wolters <typo3@wouterwolters.nl>
2012-09-04 19:06:49 +0200

    [TASK] Namespace and move t3lib unit tests - 5
    This patch moves most unit tests of the tests/Unit/t3lib/*
    directory to the core extension, following the new name scheme.
    
    Change-Id: Ia08a3c0251c577676e3920dc880bb54e1651e5c2
    Resolves: #40620
    Related: #40095
    Releases: 6.0
    Reviewed-on: http://review.typo3.org/14339
    Reviewed-by: Christian Kuhn
    Tested-by: Christian Kuhn

---
+++ Tests/Unit/Tree/Pagetree/DataProviderTest.php
@@ -0,0 +1,126 @@
+<?php
+namespace TYPO3\CMS\Backend\Tests\Unit\Tree\Pagetree;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Stefan Galinski <stefan.galinski@gmail.com>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+/**
+ * Testcase for class t3lib_tree_pagetree_DataProvider.
+ *
+ * @author Stefan Galinski <stefan.galinski@gmail.com>
+ * @author Oliver Klee <typo3-coding@oliverklee.de>
+ * @package TYPO3
+ * @subpackage t3lib
+ */
+class DataProviderTest extends \tx_phpunit_testcase {
+
+	/**
+	 * @var boolean
+	 */
+	protected $backupGlobals = TRUE;
+
+	/**
+	 * Excludes TYPO3_DB from backup/restore of $GLOBALS because resource types cannot be handled during serializing.
+	 *
+	 * @var array
+	 */
+	protected $backupGlobalsBlacklist = array('TYPO3_DB');
+
+	/**
+	 * @var \TYPO3\CMS\Backend\Tree\Pagetree\DataProvider|\PHPUnit_Framework_MockObject_MockObject
+	 */
+	protected $fixture = NULL;
+
+	public function setUp() {
+		$GLOBALS['TYPO3_CONF_VARS']['BE']['pageTree']['preloadLimit'] = 0;
+		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/tree/pagetree/class.t3lib_tree_pagetree_dataprovider.php']['postProcessCollections'] = array();
+		$this->fixture = new \TYPO3\CMS\Backend\Tree\Pagetree\DataProvider();
+	}
+
+	public function tearDown() {
+		unset($this->fixture);
+	}
+
+	/**
+	 * @test
+	 */
+	public function getRootNodeReturnsNodeWithRootId() {
+		$this->assertSame('root', $this->fixture->getRoot()->getId());
+	}
+
+	/**
+	 * @test
+	 */
+	public function getRootNodeReturnsExpandedNode() {
+		$this->assertTrue($this->fixture->getRoot()->isExpanded());
+	}
+
+	/**
+	 * @test
+	 */
+	public function getNodesSetsIsMountPointField() {
+		$subpages = array(
+			array(
+				'uid' => 1,
+				'isMountPoint' => FALSE
+			),
+			array(
+				'uid' => 2,
+				'isMountPoint' => TRUE
+			),
+			array(
+				'uid' => 3
+			)
+		);
+		$subpagesWithWorkspaceOverlay = array(
+			array(
+				'uid' => 1,
+				'title' => 'Home'
+			),
+			array(
+				'uid' => 2,
+				'title' => 'service'
+			),
+			array(
+				'uid' => 3,
+				'title' => 'contact'
+			)
+		);
+		$fixture = $this->getMock('TYPO3\\CMS\\Backend\\Tree\\Pagetree\\DataProvider', array('getSubpages', 'getRecordWithWorkspaceOverlay'));
+		$fixture->expects($this->once())->method('getSubpages')->will($this->returnValue($subpages));
+		$fixture->expects($this->at(1))->method('getRecordWithWorkspaceOverlay')->with(1)->will($this->returnValue($subpagesWithWorkspaceOverlay[0]));
+		$fixture->expects($this->at(2))->method('getRecordWithWorkspaceOverlay')->with(2)->will($this->returnValue($subpagesWithWorkspaceOverlay[1]));
+		$fixture->expects($this->at(3))->method('getRecordWithWorkspaceOverlay')->with(3)->will($this->returnValue($subpagesWithWorkspaceOverlay[2]));
+		$node = new \TYPO3\CMS\Backend\Tree\TreeNode();
+		$node->setId(12);
+		$nodeCollection = $fixture->getNodes($node);
+		$isMountPointResult = array();
+		/** @var $node \TYPO3\CMS\Backend\Tree\Pagetree\PagetreeNode */
+		foreach ($nodeCollection as $node) {
+			$isMountPointResult[] = $node->isMountPoint();
+		}
+		$this->assertSame(array(FALSE, TRUE, FALSE), $isMountPointResult);
+	}
+
+}
+
+?>
---
+++ Tests/Unit/Tree/SortedTreeNodeCollectionTest.php
@@ -0,0 +1,96 @@
+<?php
+namespace TYPO3\CMS\Backend\Tests\Unit\Tree;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2010-2011 Stefan Galinski <stefan.galinski@gmail.com>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Testcase for class \TYPO3\CMS\Backend\Tree\SortedTreeNodeCollection.
+ *
+ * @author Stefan Galinski <stefan.galinski@gmail.com>
+ * @package TYPO3
+ * @subpackage t3lib
+ */
+class SortedTreeNodeCollectionTest extends \tx_phpunit_testcase {
+
+	protected function createTestCollection() {
+		$nodeCollection = new \TYPO3\CMS\Backend\Tree\SortedTreeNodeCollection();
+		$node = new \TYPO3\CMS\Backend\Tree\TreeNode(array('id' => 5));
+		$nodeCollection->append($node);
+		$node = new \TYPO3\CMS\Backend\Tree\TreeNode(array('id' => 15));
+		$nodeCollection->append($node);
+		$node = new \TYPO3\CMS\Backend\Tree\TreeNode(array('id' => 3));
+		$nodeCollection->append($node);
+		return $nodeCollection;
+	}
+
+	protected function createTestCollectionWithTwoNodes() {
+		$nodeCollection = new \TYPO3\CMS\Backend\Tree\SortedTreeNodeCollection();
+		$node = new \TYPO3\CMS\Backend\Tree\TreeNode(array('id' => 5));
+		$nodeCollection->append($node);
+		$node = new \TYPO3\CMS\Backend\Tree\TreeNode(array('id' => 3));
+		$nodeCollection->append($node);
+		return $nodeCollection;
+	}
+
+	/**
+	 * @test
+	 */
+	public function appendsSorted() {
+		$nodeCollection = $this->createTestCollection();
+		$expected = array(3, 5, 15);
+		$ids = array();
+		foreach ($nodeCollection as $node) {
+			$ids[] = $node->getId();
+		}
+		$this->assertSame($expected, $ids);
+	}
+
+	/**
+	 * @test
+	 */
+	public function collectionContainsNode() {
+		$nodeCollection = $this->createTestCollection();
+		$node = new \TYPO3\CMS\Backend\Tree\TreeNode(array('id' => 5));
+		$this->assertTrue($nodeCollection->contains($node));
+	}
+
+	/**
+	 * @test
+	 */
+	public function searchDataWithBinarySearch() {
+		$nodeCollection = $this->createTestCollection();
+		$node = new \TYPO3\CMS\Backend\Tree\TreeNode(array('id' => 15));
+		$this->assertTrue($nodeCollection->contains($node));
+		$node = new \TYPO3\CMS\Backend\Tree\TreeNode(array('id' => 99));
+		$this->assertFalse($nodeCollection->contains($node));
+		$nodeCollection = $this->createTestCollectionWithTwoNodes();
+		$node = new \TYPO3\CMS\Backend\Tree\TreeNode(array('id' => 3));
+		$this->assertTrue($nodeCollection->contains($node));
+		$node = new \TYPO3\CMS\Backend\Tree\TreeNode(array('id' => 99));
+		$this->assertFalse($nodeCollection->contains($node));
+	}
+
+}
+
+?>

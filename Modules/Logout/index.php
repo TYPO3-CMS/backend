17cd5139f2e595b88a18a1417b6d56979687f021
Wouter Wolters <typo3@wouterwolters.nl>
2015-03-01 14:50:48 +0100

    [TASK] Make logout.php dispatched
    Use dispatching for logout.php
    
    Resolves: #65291
    Releases: master
    Change-Id: Ic003ec6dda083f69da2aaf0373ad48ea3bf4f56f
    Reviewed-on: http://review.typo3.org/37411
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Helmut Hummel <helmut.hummel@typo3.org>
    Tested-by: Helmut Hummel <helmut.hummel@typo3.org>

---
+++ Modules/Logout/index.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+/**
+ * Logout script for the backend
+ * This script saves the interface positions and calls the closeTypo3Windows in the frameset
+ *
+ * @author Kasper Skårhøj <kasperYYYY@typo3.com>
+ */
+
+$logoutController = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Backend\Controller\LogoutController::class);
+$logoutController->logout();

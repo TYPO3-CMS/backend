2f05ed214ddc5167b9f08a48a9628487682d9058
Wouter Wolters <typo3@wouterwolters.nl>
2015-03-06 19:19:53 +0100

    [TASK] Make alt_file_navframe.php dispatched
    Resolves: #65293
    Releases: master
    Change-Id: I4528cdeb58b29ed5528222485254bcc7aba73dc3
    Reviewed-on: http://review.typo3.org/37611
    Reviewed-by: Jan Helke <typo3@helke.de>
    Tested-by: Jan Helke <typo3@helke.de>
    Reviewed-by: Benjamin Mack <benni@typo3.org>
    Tested-by: Benjamin Mack <benni@typo3.org>

---
+++ Modules/FileSystemNavigationFrame/index.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+/**
+ * Folder tree in the File main module.
+ *
+ * @author Kasper Skårhøj <kasperYYYY@typo3.com>
+ */
+
+// Make instance if it is not an AJAX call
+if (!(TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_AJAX)) {
+	$fileSystemNavigationFrameController = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Backend\Controller\FileSystemNavigationFrameController::class);
+	$fileSystemNavigationFrameController->initPage();
+	$fileSystemNavigationFrameController->main();
+	$fileSystemNavigationFrameController->printContent();
+}

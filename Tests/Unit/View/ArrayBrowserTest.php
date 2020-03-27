504c4f797df7989c6e39cd4c8156cccb9d054296
Benni Mack <benni@typo3.org>
2020-03-27 11:43:04 +0100

    [TASK] Move ArrayBrowser to EXT:backend
    Resolves: #90851
    Releases: master
    Change-Id: I472845920d31449164a632b0edae442dd2b56f7d
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/63945
    Tested-by: Georg Ringer <georg.ringer@gmail.com>
    Tested-by: TYPO3com <noreply@typo3.com>
    Tested-by: Susanne Moog <look@susi.dev>
    Reviewed-by: Georg Ringer <georg.ringer@gmail.com>
    Reviewed-by: Susanne Moog <look@susi.dev>

---
+++ Tests/Unit/View/ArrayBrowserTest.php
@@ -0,0 +1,44 @@
+<?php
+namespace TYPO3\CMS\Backend\Tests\Unit\View;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Backend\View\ArrayBrowser;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+class ArrayBrowserTest extends UnitTestCase
+{
+    protected $resetSingletonInstances = true;
+
+    ///////////////////////////////
+    // Tests concerning depthKeys
+    ///////////////////////////////
+    /**
+     * @test
+     */
+    public function depthKeysWithEmptyFirstParameterAddsNothing()
+    {
+        $subject = new ArrayBrowser();
+        self::assertEquals([], $subject->depthKeys([], []));
+    }
+
+    /**
+     * @test
+     */
+    public function depthKeysWithNumericKeyAddsOneNumberForKeyFromFirstArray()
+    {
+        $subject = new ArrayBrowser();
+        self::assertEquals([0 => 1], $subject->depthKeys(['foo'], []));
+    }
+}

480b8a8c2d5cb14f34e961e127f14f78ed49faf4
Oliver Bartsch <bo@cedev.de>
2021-06-28 17:04:37 +0200

    [BUGFIX] Extract switch user handling from EXT:beuser
    The switch user handling was previously placed in EXT:beuser,
    more precisely in the BackendUserControllers' indexAction.
    It was therefore necessary to link to the extbase based
    controller with a mix of prefixed arguments and the non
    prefixed "switchUser" argument. Latter was internally
    evaluated with GU:_GP(). This means, switching users was
    done via GET requests in an extbase action, but only in
    case a non extbase prefixed argument was set.
    
    In #94209 this got worse when the BackendUserGroupController
    was merged into BackendUserController. Since this controller
    features a "remember my last action" functionality, switch user
    could no longer reliably be triggered. The evaluation of the
    non extbase prefixed "switchUser" argument only took place in
    the "user listing" (indexAction) and the success of the call
    therefore depended on the users' last called action, as this
    action was automatically used.
    
    This patch now extracts the switch user functionality from
    EXT:beuser into a dedicated EXT:backend controller, featuring
    two ajax routes "/switch/user" and "/switch/user/exit". Both
    accessible via POST requests only. To trigger those requests,
    a new JS module "TYPO3/CMS/Backend/SwitchUser" is introduced.
    This also allows to completely remove the logout hook by using
    the concrete markup (custom element) for the "exit" button.
    
    Resolves: #94426
    Related: #94209
    Releases: master
    Change-Id: I556b323fe6ae77cf696e7e34dbbe269eb4f9927a
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/69635
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Christian Kuhn <lolli@schwarzbu.ch>
    Tested-by: Benni Mack <benni@typo3.org>
    Reviewed-by: Christian Kuhn <lolli@schwarzbu.ch>
    Reviewed-by: Benni Mack <benni@typo3.org>

---
+++ Tests/Unit/Controller/SwitchUserControllerTest.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Backend\Tests\Unit\Controller;
+
+use TYPO3\CMS\Backend\Controller\SwitchUserController;
+use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+/**
+ * Test case
+ */
+class SwitchUserControllerTest extends UnitTestCase
+{
+    /**
+     * Same as in SwitchUserController
+     */
+    protected const RECENT_USERS_LIMIT = 3;
+
+    protected SwitchUserController $subject;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $GLOBALS['BE_USER'] = $this->createMock(BackendUserAuthentication::class);
+        $GLOBALS['BE_USER']->uc = [
+            'recentSwitchedToUsers' => []
+        ];
+
+        $this->subject = $this->getAccessibleMock(SwitchUserController::class, ['dummy'], [], '', false);
+    }
+
+    /**
+     * @test
+     */
+    public function generateListOfLatestSwitchedUsersReturnsCorrectAmountAndOrder(): void
+    {
+        $items = range(1, self::RECENT_USERS_LIMIT + 5);
+        $expected = array_reverse(array_slice($items, -self::RECENT_USERS_LIMIT));
+        foreach ($items as $id) {
+            $GLOBALS['BE_USER']->uc['recentSwitchedToUsers'] = $this->subject->_call('generateListOfMostRecentSwitchedUsers', $id);
+        }
+
+        self::assertCount(self::RECENT_USERS_LIMIT, $GLOBALS['BE_USER']->uc['recentSwitchedToUsers']);
+        self::assertSame($expected, $GLOBALS['BE_USER']->uc['recentSwitchedToUsers']);
+    }
+
+    /**
+     * @test
+     */
+    public function listOfLatestSwitchedUsersDoesNotContainTheSameUserTwice(): void
+    {
+        $GLOBALS['BE_USER']->uc['recentSwitchedToUsers'] = $this->subject->_call('generateListOfMostRecentSwitchedUsers', 100);
+        $GLOBALS['BE_USER']->uc['recentSwitchedToUsers'] = $this->subject->_call('generateListOfMostRecentSwitchedUsers', 100);
+
+        self::assertCount(1, $GLOBALS['BE_USER']->uc['recentSwitchedToUsers']);
+    }
+}

031b49b99488e291fbb5972e7f14112b414728b7
Benjamin Franzke <ben@bnf.dev>
2026-02-20 13:18:44 +0100

    [TASK] Allow JavaScript label API usage in install tool
    The install tool makes use of certain JavaScript components
    for EXT:backend, which requires to allow resolution of '~labels/*'
    imports for these components.
    
    A designated label provider now allows to fetch labels
    from core, backend and install. Other extensions are not allowed.
    
    Available labels need to be configured explicitly, as we can
    not use path based forward trailing-slash imports (which require
    rewrite rules) since we can not rely on rewrite rules to be available
    when TYPO3 is initially installed or in case the maintenance tool
    needs to be used in a otherwise broken installation.
    
    Releases: main
    Resolves: #109059
    Change-Id: Ifff3bf41d1c2528d443b2ab402733163ee5429f9
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/92949
    Reviewed-by: Sven Liebert <mail@sven-liebert.de>
    Tested-by: Sven Liebert <mail@sven-liebert.de>
    Reviewed-by: Benjamin Franzke <ben@bnf.dev>
    Tested-by: Benjamin Franzke <ben@bnf.dev>
    Tested-by: core-ci <typo3@b13.com>
    Reviewed-by: Benni Mack <benni@typo3.org>
    Tested-by: Benni Mack <benni@typo3.org>

--- Classes/Controller/JavaScriptLanguageDomainController.php
+++ Classes/Controller/JavaScriptLanguageDomainController.php
@@ -17,14 +17,11 @@ declare(strict_types=1);
 
 namespace TYPO3\CMS\Backend\Controller;
 
-use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Message\StreamFactoryInterface;
 use TYPO3\CMS\Backend\Attribute\AsController;
 use TYPO3\CMS\Backend\Routing\RouteResult;
-use TYPO3\CMS\Core\Localization\LanguageServiceFactory;
-use TYPO3\CMS\Core\Localization\TranslationDomainMapper;
+use TYPO3\CMS\Core\Localization\JavaScriptLanguageDomainProvider;
 
 /**
  * @internal
@@ -33,10 +30,7 @@ use TYPO3\CMS\Core\Localization\TranslationDomainMapper;
 final readonly class JavaScriptLanguageDomainController
 {
     public function __construct(
-        private LanguageServiceFactory $languageServiceFactory,
-        private TranslationDomainMapper $translationDomainMapper,
-        private ResponseFactoryInterface $responseFactory,
-        private StreamFactoryInterface $streamFactory,
+        private JavaScriptLanguageDomainProvider $javaScriptLanguageDomainProvider,
     ) {}
 
     public function getLanguageDomainAction(ServerRequestInterface $request): ResponseInterface
@@ -45,33 +39,6 @@ final readonly class JavaScriptLanguageDomainController
         $routing = $request->getAttribute('routing');
         $domain = $routing['domain'];
         $locale = $routing['locale'];
-        $languageService = $this->languageServiceFactory->create($locale);
-        $error = '';
-        $allLabels = [];
-
-        if (!$this->translationDomainMapper->isValidDomainName($domain)) {
-            // @todo check for unavailable domain name and deprecation state
-            $error = 'throw new Error("Invalid domain name");';
-        } else {
-            $allLabels = $languageService->getLabelsFromResource($domain);
-        }
-
-        $javaScriptModuleContents = implode("\n", [
-            'import { LabelProvider } from "@typo3/backend/localization/label-provider.js";',
-            $error,
-            'export default new LabelProvider(',
-            '    ' . json_encode((object)$allLabels),
-            ');',
-        ]);
-
-        $lifetime = 3600 * 24 * 365;
-        return $this->responseFactory->createResponse()
-            ->withHeader('Content-Type', 'text/javascript')
-            ->withHeader('Content-Length', (string)strlen($javaScriptModuleContents))
-            ->withHeader('Expires', gmdate('D, d M Y H:i:s T', (min($GLOBALS['EXEC_TIME'] + $lifetime, PHP_INT_MAX))))
-            ->withHeader('Cache-Control', 'private, max-age=' . $lifetime)
-            ->withBody(
-                $this->streamFactory->createStream($javaScriptModuleContents)
-            );
+        return $this->javaScriptLanguageDomainProvider->createLanguageDomainResponse($domain, $locale);
     }
 }

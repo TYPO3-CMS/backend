383f50e1308c529e15c7350977dbda593a61abba
Tobias Wojtylak <twojtylak@dfau.de>
2026-03-24 17:13:11 +0100

    [BUGFIX] Protect step methods when loading dynamic wizard steps
    Resolves: #109349
    Related: #108915
    Releases: main
    Change-Id: I62a50167d7d309fd548d556145229aa0d0702683
    Reviewed-on: https://review.typo3.org/c/Packages/TYPO3.CMS/+/93336
    Reviewed-by: Benjamin Franzke <ben@bnf.dev>
    Tested-by: core-ci <typo3@b13.com>
    Tested-by: Benjamin Franzke <ben@bnf.dev>
    Tested-by: Benjamin Kott <benjamin.kott@outlook.com>
    Reviewed-by: Benjamin Kott <benjamin.kott@outlook.com>

--- Resources/Public/JavaScript/wizard/helper/dynamic-steps-loader.js
+++ Resources/Public/JavaScript/wizard/helper/dynamic-steps-loader.js
@@ -10,4 +10,4 @@
  *
  * The TYPO3 project - inspiring people to share!
  */
-import s from"@typo3/core/ajax/ajax-request.js";function i(r,a){return new s(TYPO3.settings.ajaxUrls.wizard_config).withQueryArguments({mode:r,data:a.getDataStore()}).get().then(e=>e.resolve()).then(async e=>await Promise.all(e.steps.map(async t=>{if(!t.module)throw new Error("Step data does not contain a module path");const o=(await import(t.module)).default;if(!o)throw new Error(`Step module ${t.module} does not export a default class`);const n=new o;return Object.assign(n,{context:a,...t.configurationData}),n})))}export{i as loadDynamicSteps};
+import n from"@typo3/core/ajax/ajax-request.js";function i(r,a){return new n(TYPO3.settings.ajaxUrls.wizard_config).withQueryArguments({mode:r,data:a.getDataStore()}).get().then(e=>e.resolve()).then(async e=>await Promise.all(e.steps.map(async t=>{if(!t.module)throw new Error("Step data does not contain a module path");const{default:o}=await import(t.module);if(!o)throw new Error(`Step module ${t.module} does not export a default class`);return new o(a,t.configurationData)})))}export{i as loadDynamicSteps};
